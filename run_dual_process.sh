30fe6fd45fefcbbe4b5ae68bc61b99187af3798f
Sano Yuma <sano.yuma.c4@elms.hokudai.ac.jp>
2025-07-08 00:13:26 +0900

    bird_car

--- run_dual_process.sh
+++ run_dual_process.sh
@@ -2,7 +2,7 @@
 
 pipe=sana
 vlm=idefics2
-dataset=five_apple
+dataset=blue_bird_car
 
 save_folder=runs/${dataset}_${pipe}_${vlm}
 python3 script_dual_process.py configs/base.yaml,configs/pipe/${pipe}.yaml,configs/vlm/${vlm}.yaml,configs/dataset/${dataset}.yaml save_folder=${save_folder}
